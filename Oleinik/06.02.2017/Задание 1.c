d503dc13e82b0320b1ba8dae29fc06c45eb4ddcb
Unknown <ollmirik@gmail.com>
2017-02-16 00:07:04 +0300

    Задание 1 и 2 от 06.02.2017
    Создана папка с датой домашнего задания.

---
+++ Oleinik/06.02.2017/Задание 1.c
@@ -0,0 +1,100 @@
+﻿//Задание 1:
+//В одномерном массиве, состоящем из N вещественных чисел вычислить :
+//• Сумму отрицательных элементов
+//• Произведение элементов, находящихся между min и max элементами
+//• Произведение элементов с четными номерами
+//• Сумму элементов, находящихся между первым и последним отрицательными
+//элементами.
+
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+ 
+#define KOLLICHESTVO_CIFR_V_MASSIVE 10
+
+int funkciiaDlaSuumiOtricatelnihZnachenii(int massiv[]);
+int funkciiaDlaProizvediniaElementovMezhdyMinIMax(int massiv[]);
+
+int main()
+{
+	setlocale(LC_ALL ,"Rus");
+	srand(time(0));
+	int massiv[KOLLICHESTVO_CIFR_V_MASSIVE];
+	for (int i = 0; i < KOLLICHESTVO_CIFR_V_MASSIVE; i++)
+	{
+		massiv[i] = -5 + rand() % 10;
+		printf("%i ", massiv[i]);
+	}
+	
+	printf("\nСумма всех отричательных чисел в комбинации: %i\n", funkciiaDlaSuumiOtricatelnihZnachenii(massiv));
+	printf("Произведение чисел между минимальным и максимальным значениями: %i\n", funkciiaDlaProizvediniaElementovMezhdyMinIMax(massiv));
+}
+
+int funkciiaDlaSuumiOtricatelnihZnachenii(int massiv[])
+{
+	int peremennaDlaCymmiOtricatelnihChisel = 0;
+	for (int j = 0; j < KOLLICHESTVO_CIFR_V_MASSIVE; j++)
+	{
+		if (massiv[j] < 0)
+		{
+			peremennaDlaCymmiOtricatelnihChisel = peremennaDlaCymmiOtricatelnihChisel + massiv[j];
+		}
+	}
+	return peremennaDlaCymmiOtricatelnihChisel;
+}
+
+int funkciiaDlaProizvediniaElementovMezhdyMinIMax(int massiv[])
+{
+	int maxZnachenie = massiv[0];
+	int minZnachenie = massiv[0];
+	int chisloDlaOpredNomeraMassivaMax1;
+	int chisloDlaOpredNomeraMassivaMin1;
+
+	for (int k = 0; k < KOLLICHESTVO_CIFR_V_MASSIVE; k++)
+	{
+		if (massiv[k] >= maxZnachenie)
+		{
+			maxZnachenie = massiv[k];
+			if (maxZnachenie == massiv[k])
+			{
+				chisloDlaOpredNomeraMassivaMax1 = k;
+			}
+		}
+		if (massiv[k] <= minZnachenie)
+		{
+			minZnachenie = massiv[k];
+			if (minZnachenie == massiv[k])
+			{
+				chisloDlaOpredNomeraMassivaMin1 = k;
+			}
+		}
+	}
+	printf("Максимальное число: %i\n", maxZnachenie);
+	printf("Минимальное число: %i\n", minZnachenie);
+
+	int chisloDlaOpredNomeraMassivaMax = chisloDlaOpredNomeraMassivaMax1;
+	printf("Максимальный массив: %i\n", chisloDlaOpredNomeraMassivaMax);
+
+	int chisloDlaOpredNomeraMassivaMin = chisloDlaOpredNomeraMassivaMin1;
+	printf("Минимальный массив: %i\n", chisloDlaOpredNomeraMassivaMin);
+
+	int ProizvediniaElementovMezhdyMinIMax = 1;
+	if (chisloDlaOpredNomeraMassivaMax > chisloDlaOpredNomeraMassivaMin)
+	{
+		printf("Работа с максимума до минимума.\n");
+		for (int i = chisloDlaOpredNomeraMassivaMin; i < chisloDlaOpredNomeraMassivaMax + 1; i++)
+		{
+			ProizvediniaElementovMezhdyMinIMax = (ProizvediniaElementovMezhdyMinIMax) * (massiv[i]);
+		}
+		return ProizvediniaElementovMezhdyMinIMax;
+	}
+	if (chisloDlaOpredNomeraMassivaMin > chisloDlaOpredNomeraMassivaMax)
+	{
+		printf("Работа с минимума до максимума.\n");
+		for (int i = chisloDlaOpredNomeraMassivaMax; i < chisloDlaOpredNomeraMassivaMin + 1; i++)
+		{
+			ProizvediniaElementovMezhdyMinIMax = (ProizvediniaElementovMezhdyMinIMax) * massiv[i];
+		}
+		return ProizvediniaElementovMezhdyMinIMax;
+	}
+}
---
+++ Oleinik/06.02.2017/Задание 2.c
@@ -0,0 +1,73 @@
+﻿//Пользователь вводит прибыль фирмы за год(12 месяцев).Затем пользователь
+//вводит диапазон(например, 3 и 6 – поиск между 3 - м и 6 - м месяцем).Необходимо
+//определить месяц, в котором прибыль была максимальна и месяц, в котором прибыль
+//была минимальна с учетом выбранного диапазона.
+
+#include <stdio.h>
+#include <locale.h>
+
+int funkciaDlaPodschetaMaxZnachenia(int massivPribileIliYbitkovPredpiiatia[], int nachaloDiapozonaPoiska, int konecDiapozonaPoiska);
+int funkciaDlaPodschetaMinZnachenia(int massivPribileIliYbitkovPredpiiatia[], int nachaloDiapozonaPoiska, int konecDiapozonaPoiska);
+
+
+int main()
+{
+	int massivPribileIliYbitkovPredpiiatia[12];
+	setlocale(LC_ALL, "Rus");
+	for (int i = 0; i < 12; i++)
+	{
+		printf("Введите пожалуйста доход за %i месяц : ", i + 1);
+		massivPribileIliYbitkovPredpiiatia[i];
+		scanf("%i", &massivPribileIliYbitkovPredpiiatia[i]);
+	}
+
+
+	int nachaloDiapozonaPoiska;
+	int konecDiapozonaPoiska;
+	printf("Задайте начало диапозона: ");
+	scanf("%i", &nachaloDiapozonaPoiska);
+	printf("Задайте конец диапозона: ");
+	scanf("%i", &konecDiapozonaPoiska);
+	if (nachaloDiapozonaPoiska < 1 || nachaloDiapozonaPoiska > 12 || konecDiapozonaPoiska > 12 || konecDiapozonaPoiska < 1)
+	{
+		printf("Ошибка ввода значений.\n");
+		return 0;
+	}
+	else if (nachaloDiapozonaPoiska > konecDiapozonaPoiska)
+	{
+		printf("Мы обнаружили, что начало диапозона больше его конца. Зачения были исправлены в ходе программы местами.\n");
+		int chisloDlaZameniMestami = nachaloDiapozonaPoiska;
+		nachaloDiapozonaPoiska = konecDiapozonaPoiska;
+		konecDiapozonaPoiska = chisloDlaZameniMestami;
+	}
+	
+	printf("Максимальная прибыль за данный период была: %i\n", funkciaDlaPodschetaMaxZnachenia(massivPribileIliYbitkovPredpiiatia, nachaloDiapozonaPoiska, konecDiapozonaPoiska));
+
+	printf("Минимальная прибыль за данный период была: %i\n", funkciaDlaPodschetaMinZnachenia(massivPribileIliYbitkovPredpiiatia, nachaloDiapozonaPoiska, konecDiapozonaPoiska));
+}
+
+int funkciaDlaPodschetaMaxZnachenia(int massivPribileIliYbitkovPredpiiatia[], int nachaloDiapozonaPoiska, int konecDiapozonaPoiska)
+{
+	int max = massivPribileIliYbitkovPredpiiatia[nachaloDiapozonaPoiska];
+	for (int i = nachaloDiapozonaPoiska - 1; i < konecDiapozonaPoiska; i++)
+	{
+		if (max < massivPribileIliYbitkovPredpiiatia[i])
+		{
+			max = massivPribileIliYbitkovPredpiiatia[i];
+		}
+	}
+	return max;
+}
+
+int funkciaDlaPodschetaMinZnachenia(int massivPribileIliYbitkovPredpiiatia[], int nachaloDiapozonaPoiska, int konecDiapozonaPoiska)
+{
+	int min = massivPribileIliYbitkovPredpiiatia[nachaloDiapozonaPoiska];
+	for (int i = nachaloDiapozonaPoiska - 1; i < konecDiapozonaPoiska; i++)
+	{
+		if (min > massivPribileIliYbitkovPredpiiatia[i])
+		{
+			min = massivPribileIliYbitkovPredpiiatia[i];
+		}
+	}
+	return min;
+}
