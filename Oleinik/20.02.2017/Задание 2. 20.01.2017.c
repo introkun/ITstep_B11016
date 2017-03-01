41c9b138c951161dde06b7d2d5f7a2583098d6c4
Unknown <ollmirik@gmail.com>
2017-03-01 23:28:03 +0300

    Домашнее задание за 20.02.2017

---
+++ Oleinik/20.02.2017/Задание 2. 20.01.2017.c
@@ -0,0 +1,30 @@
+﻿//Задание 2:
+//Напишите программу возведения чисел в степень, используйте указатели для передачи результирующего значения в функцию и из нее.
+//Числа необходимо получать от пользователя.
+
+#include <stdio.h>
+#include <locale.h>
+
+void getDegreeForUserNumber(int*userNumber, int* degreesNumber, int* answerForUser);
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	int userNumber;
+	printf("Программа высчитывает степень числа введенные с клавиатуры.\nВведите желаемое число: ");
+	scanf("%i", &userNumber);
+	int degreesNumber;
+	printf("Введите степень для числа: ");
+	scanf("%i", &degreesNumber);
+	int answerForUser = 1;
+	getDegreeForUserNumber(&userNumber, &degreesNumber, &answerForUser);
+	printf("%i\n", answerForUser);
+}
+
+void getDegreeForUserNumber(int* userNumber, int* degreesNumber, int* answerForUser)
+{
+	for (int i = 0; i < *degreesNumber; i++)
+	{
+		*answerForUser *= *userNumber;
+	}
+}
