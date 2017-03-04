c384dce8ccfcf6d3e6db7b145e85b90ea1769646
Unknown <ollmirik@gmail.com>
2017-03-04 20:28:25 +0300

    Домашнее задание за 27.02.2017

---
+++ Oleinik/27.02.2017/Задание 1. 27.02.2017.c
@@ -0,0 +1,186 @@
+﻿//Задание 1
+//Написать функцию, которая получает указатель на статический двумерный массив(3x5) и
+//его размер.Функция распределяет положительные, отрицательные и нулевые элементы в
+//3 отдельных массива, указатели на которые также переданы в функцию.
+
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+
+#define ARRAY_ROW 3
+#define ARRAY_COLUMN 5
+
+void fillMainArray(int(*pArray)[ARRAY_COLUMN], int arrayRow, int arrayColumn);
+void transferNumbersFromMainArrayToAuxiliaryArray(int(*pArray)[ARRAY_COLUMN], int arrayRow, int arrayColumn, int(*pArrayForNegNumbers)[ARRAY_COLUMN], int(*pArrayForPosNumbers)[ARRAY_COLUMN], int(*pArrayForZero)[ARRAY_COLUMN]);
+enum ChooseBetweenPosNegZero
+{
+	PosNumbers = 1,
+	NegNumbers,
+	ZeroNumbers
+};
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	srand(time(0));
+	int array[ARRAY_ROW][ARRAY_COLUMN];
+	int arrayForNegNumbers[ARRAY_ROW][ARRAY_COLUMN];
+	int arrayForPosNumbers[ARRAY_ROW][ARRAY_COLUMN];
+	int arrayForZero[ARRAY_ROW][ARRAY_COLUMN];
+	fillMainArray(array, ARRAY_ROW, ARRAY_COLUMN);
+	transferNumbersFromMainArrayToAuxiliaryArray(array, ARRAY_ROW, ARRAY_COLUMN, arrayForNegNumbers, arrayForPosNumbers, arrayForZero);
+
+}
+
+void fillMainArray(int(*pArray)[ARRAY_COLUMN], int arrayRow, int arrayColumn)
+{
+	printf("Ваша матрица имеет следующий вид:\n");
+	for (int i = 0; i < arrayRow; i++)
+	{
+		for (int j = 0; j < arrayColumn; j++)
+		{
+			*(*(pArray + i) + j) = -5 + rand() % 10;
+			printf("%i ", *(*(pArray + i) + j));
+		}
+		printf("\n");
+	}
+}
+
+void transferNumbersFromMainArrayToAuxiliaryArray(int(*pArray)[ARRAY_COLUMN], int arrayRow, int arrayColumn, int(*pArrayForNegNumbers)[ARRAY_COLUMN], int(*pArrayForPosNumbers)[ARRAY_COLUMN], int(*pArrayForZero)[ARRAY_COLUMN])
+{
+	int countForPosNumbers = 0;
+	int countForNegNumbers = 0;
+	int countForZero = 0;
+
+	int posRow = 0;
+	int posColumn = 0;
+	int negRow = 0;
+	int negColumn = 0;
+	int zeroRow = 0;
+	int zeroColumn = 0;
+
+	for (int k = 0; k < arrayRow; k++)
+	{
+		for (int l = 0; l < arrayColumn; l++)
+		{
+			if (*(*(pArray + k) + l) > 0)
+			{
+				*(*(pArrayForPosNumbers + posRow) + posColumn) = *(*(pArray + k) + l);
+				countForPosNumbers++;
+				posColumn++;
+				if (posColumn == 5)
+				{
+					posRow++;
+					posColumn = 0;
+				}
+			}
+		}
+	}
+	for (int m = 0; m < arrayRow; m++)
+	{
+		for (int n = 0; n < arrayColumn; n++)
+		{
+			if (*(*(pArray + m) + n) < 0)
+			{
+				*(*(pArrayForNegNumbers + negRow) + negColumn) = *(*(pArray + m) + n);
+				countForNegNumbers++;
+				negColumn++;
+				if (negColumn == 5)
+				{
+					negRow++;
+					negColumn = 0;
+				}
+			}
+		}
+	}
+	for (int o = 0; o < arrayRow; o++)
+	{
+		for (int p = 0; p < arrayColumn; p++)
+		{
+			if (*(*(pArray + o) + p) == 0)
+			{
+				*(*(pArrayForZero + zeroRow) + zeroColumn) = *(*(pArray + o) + p);
+				countForZero++;
+				zeroColumn++;
+				if (zeroColumn == 5)
+				{
+					zeroRow++;
+					zeroColumn = 0;
+				}
+			}
+		}
+	}
+
+	enum ChooseBetweenPosNegZero chooseBetweenPosNegZero;
+	printf("Какую функцию выполним:\n1. Вывод матрицы положительных чисел.\n2. Вывод матрицы отрицательных чисел.\n3. Вывод матрицы нулей.\n");
+	scanf("%i", &chooseBetweenPosNegZero);
+	switch (chooseBetweenPosNegZero)
+	{
+	case PosNumbers:
+		printf("Матрица положительных чисел будет иметь следующий вид:\n");
+		for (int m = 0; m < arrayRow; m++)
+		{
+			for (int n = 0; n < arrayColumn; n++)
+			{
+				countForPosNumbers--;
+				if (countForPosNumbers < 0)
+				{
+					break;
+				}
+				printf("%i ", *(*(pArrayForPosNumbers + m) + n));
+			}
+			printf("\n");
+			if (countForPosNumbers < 0)
+			{
+				break;
+			}
+			
+		}
+		break;
+	case NegNumbers:
+		printf("Матрица отрицательных чисел будет иметь следующий вид:\n");
+		for (int m = 0; m < arrayRow; m++)
+		{
+			for (int n = 0; n < arrayColumn; n++)
+			{
+				countForNegNumbers--;
+				if (countForNegNumbers < 0)
+				{
+					break;
+				}
+				printf("%i ", *(*(pArrayForNegNumbers + m) + n));
+			}
+			printf("\n");
+			if (countForNegNumbers < 0)
+			{
+				break;
+			}
+		}
+		break;
+	case ZeroNumbers:
+		printf("Матрица нулей будет иметь следующий вид:\n");
+		for (int m = 0; m < arrayRow; m++)
+		{
+			for (int n = 0; n < arrayColumn; n++)
+			{
+				
+				if (countForZero <= 0)
+				{
+					break;
+				}
+				countForZero--;
+				printf("%i ", *(*(pArrayForZero + m) + n));
+				
+			}
+			printf("\n");
+			if (countForZero <= 0)
+			{
+				break;
+			}
+		}
+		break;
+	default:
+		printf("Ошибка! Повторите ввод.\n");
+		break;
+	}
+}
---
+++ Oleinik/27.02.2017/Задание 2. 27.02.2017.c
@@ -0,0 +1,63 @@
+﻿//Задание 2
+//Написать программу, которая содержит функцию, принимающую в качестве аргумента,
+//указатели на три массива и размер массивов.Функция заносит в массив C суммы
+//элементов массивов А и В.Размер массивов одинаковый.
+
+#include <stdio.h>
+#include <locale.h>
+#include <stdlib.h>
+
+#define ARRAY_ROW 5
+
+void fillMainArray(int* auxiliaryArrayA, int* auxiliaryArrayB, int arrayRow);
+void additionSumOfAuxiliaryArrays(int* auxiliaryArrayA, int* auxiliaryArrayB, int* mainArray, int arrayRow);
+void printArrays(int* auxiliaryArrayA, int* auxiliaryArrayB, int* mainArray, int arrayRow);
+
+int main()
+{
+	setlocale(LC_ALL, "Rus");
+	srand(time(0));
+	int mainArray[ARRAY_ROW];
+	int auxiliaryArrayA[ARRAY_ROW];
+	int auxiliaryArrayB[ARRAY_ROW];
+	fillMainArray(auxiliaryArrayA, auxiliaryArrayB, ARRAY_ROW);
+	additionSumOfAuxiliaryArrays(auxiliaryArrayA, auxiliaryArrayB, mainArray, ARRAY_ROW);
+	printArrays(auxiliaryArrayA, auxiliaryArrayB, mainArray, ARRAY_ROW);
+}
+
+void fillMainArray(int* auxiliaryArrayA, int* auxiliaryArrayB, int arrayRow)
+{
+	for (int i = 0; i < arrayRow; i++)
+	{
+		*(auxiliaryArrayA + i) = rand() % 10;
+		*(auxiliaryArrayB + i) = rand() % 10;
+	}
+}
+
+void additionSumOfAuxiliaryArrays(int* auxiliaryArrayA, int* auxiliaryArrayB, int* mainArray, int arrayRow)
+{
+	for (int j = 0; j < arrayRow; j++)
+	{
+		*(mainArray + j) = *(auxiliaryArrayA + j) + *(auxiliaryArrayB + j);
+	}
+}
+
+void printArrays(int* auxiliaryArrayA, int* auxiliaryArrayB, int* mainArray, int arrayRow)
+{
+	printf("Первый массив:\n");
+	for (int k = 0; k < arrayRow; k++)
+	{
+		printf("%i ", *(auxiliaryArrayA + k));
+	}
+	printf("\nВторой массив:\n");
+	for (int l = 0; l < arrayRow; l++)
+	{
+		printf("%i ", *(auxiliaryArrayB + l));
+	}
+	printf("\nСумма элементов массива:\n");
+	for (int m = 0; m < arrayRow; m++)
+	{
+		printf("%i ", *(mainArray + m));
+	}
+}
+
