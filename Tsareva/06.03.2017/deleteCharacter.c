f42f1ae5645663dea30f52b8496344a07145426c
alisatsar <alisacookee@gmail.com>
2017-03-12 17:57:14 +0300

    06.03.2017

---
+++ Tsareva/06.03.2017/deleteCharacter.c
@@ -0,0 +1,62 @@
+﻿//Написать функцию, которая удаляет из строки все вхождения в нее заданного символа.
+
+#include <stdio.h>
+
+#define SIZE_STRING 500
+
+int findLength(char* str);
+void deleteChar(char* str, int* len);
+
+int main()
+{
+	char str[SIZE_STRING];
+
+	printf("Enter your text:\n");
+	fgets(str, SIZE_STRING, stdin);
+
+	int len = findLength(str);
+	
+	deleteChar(str, &len);
+}
+
+int findLength(char* str)
+{
+	int count = 0;
+
+	do{
+		count++;
+	} while (*(str + count) != 0);
+
+	return count - 1;
+}
+
+void deleteChar(char* str, int* len)
+{
+	char deleteChar;
+	printf("Enter character, what you want delete:\n");
+	scanf("%c", &deleteChar);
+
+	for (int i = 0; i < *len; i++)
+	{
+		if (*(str + 0) == deleteChar)
+		{
+			i = 0;
+		}
+		if (*(str + i) == deleteChar)
+		{
+			for (int j = i; j < *len; j++)
+			{
+				*(str + j) = str[j + 1];
+				if ((*(str + j) == '\n'))
+				{
+					*(str + j) = '\0';
+				}
+			}
+			if ((*(str + i) == deleteChar))
+			{
+				i--;
+			}
+		}
+	}
+	puts(str);
+}
---
+++ Tsareva/06.03.2017/deleteCharacterToNumber.c
@@ -0,0 +1,54 @@
+﻿//Написать функцию, которая удаляет из строки символ с заданным номером
+
+#include <stdio.h>
+
+#define SIZE_STRING 500
+
+int length(char* str);
+void deliteChar(char* str, int* len);
+
+int main()
+{
+	char str[SIZE_STRING];
+
+	printf("Enter your text:\n");
+	fgets(str, SIZE_STRING, stdin);
+
+	int len = length(str);
+
+	deliteChar(str, &len);
+}
+
+int length(char* str)
+{
+	int count = 0;
+
+	do {
+		count++;
+	} while (str[count] != 0);
+
+	return count - 1;
+}
+
+void deliteChar(char* str, int* len)
+{
+	int userDeleteChar;
+
+	printf("The text contains %i character. Enter the number of character, that you want delete:\n", *len);
+	scanf("%i", &userDeleteChar);
+	
+	while (userDeleteChar > *len)
+	{
+		printf("The text contains %i character. You are enter the number of character, that not exist.\nPlease, try again:\n", *len);
+		scanf("%i", &userDeleteChar);
+	}
+
+	for (int i = userDeleteChar - 1; i < *len; i++)
+	{
+		if (userDeleteChar > 0)
+		{
+			str[i] = str[i + 1];
+		}
+	}
+	puts(str);
+}
