9e49b2b30604876b3a248aa6cef9b23082285bb5
Unknown <ollmirik@gmail.com>
2017-03-25 17:11:21 +0300

    Домашнее задание за 06.03.2017

---
+++ Oleinik/06.03.2017/Задание 1. 06.03.2017.c
@@ -0,0 +1,54 @@
+﻿//Задание 1:
+//Написать функцию, которая удаляет из строки символ с заданным номером.
+
+#include <stdio.h>
+#include <locale.h>
+#include <string.h>
+
+#define ARRAY_ROW 1024
+
+int calcArrayLength(char* stringArray);
+int choosingSymbol(char* stringArray);
+void printstringWithDeletedSymbol(char* stringArray);
+
+int main()
+{
+	printf("This programm can delete any symbol in string row.\nEnter your sentence: ");
+	char stringArray[ARRAY_ROW];
+	fgets(stringArray, ARRAY_ROW, stdin);
+	int userNumberChoise = choosingSymbol(stringArray);
+	printstringWithDeletedSymbol(stringArray);
+
+}
+
+int calcArrayLength(char* stringArray)
+{
+	int arrayLength = 0;
+	while (*stringArray++ != 0)
+	{
+		arrayLength++;
+	}
+	return arrayLength - 1;
+}
+
+int choosingSymbol(char* stringArray)
+{
+	int arrayLength = calcArrayLength(stringArray);
+
+	printf("Your array length contains %i symbols.\nNow, you can pick a number between 0 and %i to delete any letter in your sentence: ", arrayLength, arrayLength);
+	int userNumberChoise;
+	scanf("%i", &userNumberChoise);
+	if (userNumberChoise > arrayLength || userNumberChoise < 0)
+	{
+		printf("Error! Make another choise.\n");
+		choosingSymbol(stringArray);
+	}
+	stringArray[userNumberChoise] = ' ';
+	return userNumberChoise;
+}
+
+void printstringWithDeletedSymbol(char* stringArray)
+{
+	printf("Your reformed sentence: ");
+	printf("%s", stringArray);
+}
---
+++ Oleinik/06.03.2017/Задание 2. 06.03.2017.c
@@ -0,0 +1,48 @@
+﻿//Задание 2:
+//Написать функцию, которая удаляет из строки все вхождения в нее заданного символа.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#define STRING_ARRAY_ROW 1024
+
+void printString(char* stringSymbols);
+int deleteSymbol(char* stringSymbols);
+
+int main()
+{
+	printf("Program for deleting symbols.\nEnter your sentence: ");
+	char stringSymbols[STRING_ARRAY_ROW];
+	fgets(stringSymbols, STRING_ARRAY_ROW, stdin);
+	deleteSymbol(stringSymbols);
+	printString(stringSymbols);
+}
+
+int deleteSymbol(char* stringSymbols)
+{
+	printf("Entre a symbol, that you want to delete: ");
+	char symbolForDelete;
+	scanf("%c", &symbolForDelete);
+	int count = 0;
+	for (int i = 0; i < STRING_ARRAY_ROW; i++)
+	{
+		if (symbolForDelete == stringSymbols[i])
+		{
+
+			stringSymbols[i] = ' ';
+			count++;
+		}
+	}
+	if (count == 0)
+	{
+		printf("I can't find this symbol. Please, enter another letter.\n");
+		exit(0);
+	}
+
+}
+
+void printString(char* stringSymbols)
+{
+	printf("%s", stringSymbols);
+}
