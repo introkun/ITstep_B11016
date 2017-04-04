a5ec3b27e8a72f81582377148d63b1eb1c19c97a
alisatsar <alisacookee@gmail.com>
2017-04-04 22:52:14 +0300

    27.03.2017

---
+++ Tsareva/27.03.2017/enterAndSortStudentList.c
@@ -0,0 +1,114 @@
+﻿/*Задание 1:
+Написать программу, которая дает пользователю ввести 5 фамилий студентов и хранит их
+в динамическом массиве, а затем сортирует их по возрастанию.*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+#define SIZE_LIST 5
+
+char** allocList(int sizeList);
+void allocLastName(char** list, int sizeList);
+void addList(char** list, int sizeList);
+void sort(char** list, int sizeList);
+void printList(char** list, int sizeList);
+void deleteMemory(char** list, int sizeList);
+
+int main()
+{
+	char** list = allocList(SIZE_LIST);
+
+	allocLastName(list, SIZE_LIST);
+
+	addList(list, SIZE_LIST);
+
+	sort(list, SIZE_LIST);
+
+	printList(list, SIZE_LIST);
+
+	deleteMemory(list, SIZE_LIST);
+}
+
+char** allocList(int sizeList)
+{
+	return malloc(sizeList * sizeof(char*));
+}
+
+void allocLastName(char** list, int sizeList)
+{
+	if (list == 0)
+	{
+		printf("The wrong pointer in function allocLastName. The pointer should't equal NULL\n");
+		return 0;
+	}
+	for (int i = 0; i < sizeList; i++)
+	{
+		*(list + i) = malloc(256 * sizeof(char));
+	}
+}
+
+void addList(char** list, int sizeList)
+{
+	if (list == 0)
+	{
+		printf("The wrong pointer in function addList. The pointer should't equal NULL\n");
+		return 0;
+	}
+	for (int i = 0; i < sizeList; i++)
+	{
+		printf("last name %i: \n", i + 1);
+		fgets(*(list + i), 256, stdin);
+		printf("\n");
+	}
+}
+
+void sort(char** list, int sizeList)
+{
+	if (list == 0)
+	{
+		printf("The wrong pointer in function sort. The pointer should't equal NULL\n");
+		return 0;
+	}
+	char* buff = NULL;
+	for (int i = 0; i < sizeList - 1; i++)
+	{
+		for (int j = 0; j < sizeList - i - 1; j++)
+		{
+			if (strcmp(*(list + j), *(list + j + 1)) == 1)
+			{
+				buff = *(list + j);
+				*(list + j) = *(list + j + 1);
+				*(list + j + 1) = buff;
+			}
+		}
+	}
+}
+
+void printList(char** list, int sizeList)
+{
+	if (list == 0)
+	{
+		printf("The wrong pointer in function printList. The pointer should't equal NULL\n");
+		return 0;
+	}
+	for (int i = 0; i < sizeList; i++)
+	{
+		printf("print last name %i: \n", i + 1);
+		printf("%s", *(list + i));
+	}
+}
+
+void deleteMemory(char** list, int sizeList)
+{
+	if (list == 0)
+	{
+		printf("The wrong pointer in function deleteMemory\n");
+		return 0;
+	}
+	for (int i = 0; i < sizeList; i++)
+	{
+		free(*(list + i));
+	}
+
+	free(list);
+}
---
+++ Tsareva/27.03.2017/filterCar.c
@@ -0,0 +1,130 @@
+﻿/*Задание 2:
+Написать программу, которая хранит информацию об автомобиле(марка, модель, цвет
+(enum), тип топлива(enum) и год) в динамическом массиве.Пользователь определяет
+размерность массива и вводит 5 автомобилей.Реализовать возможность поиска всех
+автомобилей младше определенного года.*/
+
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+enum Color
+{
+	Red = 1,
+	Green,
+	Black,
+	While,
+	Grey
+};
+
+enum FuelType
+{
+	Benzine80 = 1,
+	Benzine92,
+	Benzine95,
+	Gas,
+	Diesel
+};
+
+struct Car
+{
+	char* Brand;
+	char* Model;
+	enum Color Color;
+	enum FuelType FuelType;
+	int Year;
+};
+
+char* getString(char* message, char* st, int maxStringSize);
+void cleanInput();
+void getInt(char* message, int* value);
+void printCarsFilteredByYear(struct Car* cars, int carsCount, int year);
+
+int main()
+{
+	int countCar = 0;
+
+	printf("Enter the count of car: ");
+	scanf("%i", &countCar);
+	cleanInput();
+
+	struct Car* car = malloc(sizeof(struct Car) * countCar);
+
+	int i = 0;
+	while (i < countCar)
+	{
+		printf("\n=== Please enter #%i car: ===\n", i + 1);
+		car[i].Brand = malloc(30);
+		getString("Brand: ", car[i].Brand, 30);
+		car[i].Model = malloc(30);
+		getString("Model: ", car[i].Model, 30);
+		getInt("Year: ", (int*)&car[i].Year);
+		getInt("Choose the color:\nRed = 1\nGreen = 2\nBlack = 3\nWhile = 4\nGrey = 5: \n", (int*)&car[i].Color);
+		getInt("Choose the fuel type:\nBenzine80 = 1\nBenzine92 = 2\nBenzine95 = 3\nGas = 4\nDiesel = 5: \n",
+			(int*)&car[i].FuelType);
+		cleanInput();
+		i++;
+	}
+
+	int sortYear;
+	printf("Please, enter the year, from you want sort: \n");
+	scanf("%i", &sortYear);
+
+	printCarsFilteredByYear(car, countCar, sortYear);
+}
+
+char* getString(char* message, char* st, int maxStringSize)
+{
+	char* find;
+
+	printf("%s", message);
+	char* result = fgets(st, maxStringSize, stdin);
+	if (result)
+	{
+		find = strchr(st, '\n');
+		if (find)
+		{
+			*find = 0;
+		}
+		else
+		{
+			cleanInput();
+		}
+	}
+	return result;
+}
+
+void cleanInput()
+{
+	char ch;
+	do
+	{
+		ch = getchar();
+	} while (ch != '\n' && ch != EOF);
+}
+
+void getInt(char* message, int* value)
+{
+	printf("%s", message);
+	scanf("%i", value);
+}
+
+void printCar(struct Car* car)
+{
+	printf("Brand:\t%s\n", car->Brand);
+	printf("Model:\t%s\n", car->Model);
+	printf("FuelType:\t%i\n", car->FuelType);
+	printf("Color:\t%i\n", car->Color);
+	printf("Year:\t%i\n\n", car->Year);
+}
+
+void printCarsFilteredByYear(struct Car* cars, int carsCount, int year)
+{
+	for (int i = 0; i < carsCount; i++)
+	{
+		if (cars[i].Year > year)
+		{
+			printCar(&cars[i]);
+		}
+	}
+}
